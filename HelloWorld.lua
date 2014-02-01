f43683aad72ff26bf92511bb3b8bc411b4537ecc
fraggerfox <santhosh.raju@gmail.com>
2014-02-02 05:03:44 +0530

    Added a simple slash command
    Added some documentation via comments

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -1,3 +1,18 @@
+--
+-- A simple Slash command
+-- This can be used to access the Addon functions in-game
+-- by the use of a /<command> <parameters> format
+--
+SLASH_HELLO_WORLD_SHOW1 = "/hwshow"
+SLASH_HELLO_WORLD_SHOW2 = "/helloworldshow" -- an alias to /hwshow
+
+SlashCmdList["HELLO_WORLD_SHOW"] = function ()
+	HelloWorld()
+end
+
+--
+-- A simple function
+--
 function HelloWorld() 
   -- print("Hello, Universe!"); -- Simple Console Message
   message("Hello, Universe!"); -- Simple PopUp Box
