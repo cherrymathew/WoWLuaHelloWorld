10ff082240d37abfe309c66c51fd5cee39067ef3
fraggerfox <santhosh.raju@gmail.com>
2014-02-01 18:14:15 +0530

    Fixed the comment form "//" to "--"

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -1,4 +1,4 @@
 function HelloWorld() 
-   print("Hello, Universe!"); // Simple Console Message
-  //message("Hello, Universe!"); // Simple PopUp Box
+  -- print("Hello, Universe!"); -- Simple Console Message
+  message("Hello, Universe!"); -- Simple PopUp Box
 end
