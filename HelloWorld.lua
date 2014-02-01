3b144aa1d499591d36384ce0105056b798eed93d
fraggerfox <santhosh.raju@gmail.com>
2014-02-02 05:10:53 +0530

    Added a simple parameter handling for the Slash command

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -6,14 +6,19 @@
 SLASH_HELLO_WORLD_SHOW1 = "/hwshow"
 SLASH_HELLO_WORLD_SHOW2 = "/helloworldshow" -- an alias to /hwshow
 
-SlashCmdList["HELLO_WORLD_SHOW"] = function ()
-	HelloWorld()
+SlashCmdList["HELLO_WORLD_SHOW"] = function (msg)
+	HelloWorld(msg)
 end
 
 --
 -- A simple function
 --
-function HelloWorld() 
+function HelloWorld(msg) 
+	local str = str or msg;
   -- print("Hello, Universe!"); -- Simple Console Message
   message("Hello, Universe!"); -- Simple PopUp Box
+  
+  if str then
+  	message("You passed " .. msg);
+  end
 end
