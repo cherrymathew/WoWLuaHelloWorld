4407bd5c650b4bfa144dc31bb0e2f5efd1a8c7f7
fraggerfox <santhosh.raju@gmail.com>
2014-02-19 21:25:39 +0530

    Added a dropdown to select between stacks and individual

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -14,21 +14,49 @@ end
 -- References used for the current framework
 -- Button Generator - http://wowprogramming.com/snippets/LOL_CLASS_31
 -- Quantity Box - http://eu.battle.net/wow/en/forum/topic/1622897526
+-- Dropdown Box - http://wowprogramming.com/snippets/Create_UI-styled_dropdown_menu_10
 --
 local scale = 1
 local prevpos = false
 local itemlist = {};
 
+local quantityTypes = {
+	"Individual", -- ID 1
+	"Stacks", -- ID 2
+}
+
 --
 -- Advertise the Item into Trade Chat (Channel 2), when the Icon is clicked
 --
-local function advertiseItem(itemID)
+local function advertiseItem(itemID, quantity, quantityType)
 	local index, name = GetChannelName(2) -- It finds Trade is a channel at index 2
-	local quantity = _G[itemID.."-editBox"]:GetNumber()
+	local quantityString = (quantityType == 2 and " Stack(s)." or ".")
+
 	if (index ~= nil) then 
 		local itemName, itemLink = GetItemInfo(itemID)
-		SendChatMessage("WTS "..itemLink.." x "..quantity , "CHANNEL", nil, index);
+		SendChatMessage("WTS "..itemLink.." x "..quantity..quantityString, "CHANNEL", nil, index);
 	end
+end
+
+--
+-- Callback for whenever the drop down changes
+--
+local function dropdownBoxOnClick(self, frame)
+  UIDropDownMenu_SetSelectedID(frame, self:GetID())
+end
+
+--
+-- Initialization of the dropdown and filling up with values
+--
+local function initializeDropdownBox(self, level)
+	local info = UIDropDownMenu_CreateInfo()
+	for k,v in pairs(quantityTypes) do
+		info = UIDropDownMenu_CreateInfo()
+		info.text = v
+		info.value = v
+		info.func = function(this) dropdownBoxOnClick(this, self) end
+		UIDropDownMenu_AddButton(info, level)
+	end 
 end
 
 --
@@ -41,44 +69,57 @@ end
 -- 3. Implement basic trading logic (yet to be split into micro tasks)
 --
 local function generateButton(itemIcon, itemID) 
-   if itemlist[itemID] == true then
-      return;
-   end
+	if itemlist[itemID] == true then
+		return;
+	end
+	
+	local button = CreateFrame("Button", itemID.."-button", HelloWorldForm, "ActionButtonTemplate")
+	local editBox = CreateFrame("EditBox", itemID.."-editBox", HelloWorldForm, "InputBoxTemplate")
+	local dropdownBox = CreateFrame("Button", itemID.."-dropdownBox", HelloWorldForm, "UIDropDownMenuTemplate")
+	
+	-- Initialize each of the UI elements
+	button:SetScale(scale)
+	
+	editBox:SetWidth(35)
+	editBox:SetHeight(50)
+	editBox:SetAutoFocus(false)
+	editBox:SetNumeric(true)
+	editBox:SetNumber(1)
+	
+	dropdownBox:ClearAllPoints()
+	dropdownBox:SetPoint("CENTER", 0, 0)
+	dropdownBox:Show()
 
-   local button = CreateFrame("Button", itemID.."-button", HelloWorldForm, "ActionButtonTemplate")
-   local editBox = CreateFrame("EditBox", itemID.."-editBox", HelloWorldForm, "InputBoxTemplate")
-   
-   button:SetScale(scale)
-   editBox:SetWidth(35)
-   editBox:SetHeight(50)
-   editBox:SetAutoFocus(false)
-   editBox:SetNumeric(true)
-   editBox:SetNumber(1)
-   
+	UIDropDownMenu_Initialize(dropdownBox, initializeDropdownBox)
+	UIDropDownMenu_SetWidth(dropdownBox, 100);
+	UIDropDownMenu_SetButtonWidth(dropdownBox, 124)
+	UIDropDownMenu_SetSelectedID(dropdownBox, 1)
+	UIDropDownMenu_JustifyText(dropdownBox, "LEFT")
+
    if not prevpos then
    	button:SetPoint("TOPLEFT",HelloWorldForm,"TOPLEFT",13,-13)
    	editBox:SetPoint("TOPLEFT",HelloWorldForm,"TOPLEFT",65,-6.5)
+   	dropdownBox:SetPoint("TOPLEFT",HelloWorldForm,"TOPLEFT",130,-6.5)
    else 
    	button:SetPoint("TOP",prevpos,"BOTTOM",0,-4)
    	editBox:SetPoint("TOP",prevposBox,"BOTTOM",0,2)
+   	dropdownBox:SetPoint("TOP",prevposDropdownBox,"BOTTOM",0,2)
    end
 
    _G[button:GetName().."Icon"]:SetTexture(itemIcon)
    _G[button:GetName().."Icon"]:SetTexCoord(0, 1, 0, 1)   
 	
---	button:SetScript("OnClick", function()
---			 SendChatMessage(itemID,"SAY")
---	end )
-	
-	button:SetScript("OnClick", function() advertiseItem(itemID) end)
+  
+	button:SetScript("OnClick", function() advertiseItem(itemID, editBox:GetNumber(), UIDropDownMenu_GetSelectedID(dropdownBox)) end)
 
 	editBox:SetScript("OnEnterPressed", function (self) 
 				editBox:ClearFocus(); -- clears focus from editbox, (unlocks key bindings, so pressing W makes your character go forward.
 	end );
 	editBox:Show()
-	
+
 	prevpos = itemID.."-button"
 	prevposBox = itemID.."-editBox"
+	prevposDropdownBox = itemID.."-dropdownBox"
 	itemlist[itemID] = true
 end
 
@@ -154,3 +195,6 @@ function HelloWorld(msg)
   -- Sets the backdrop to be transparent
   HelloWorldForm:SetBackdrop(StaticPopup1:GetBackdrop())
 end
+
+
+
